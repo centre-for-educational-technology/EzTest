2e746a24a3b3cb30fdac5400846472f4b588202a
Pavel Djundik <github@xpaw.me>
2016-01-11 16:28:36 +0200

    Add smtp config

--- src/Config.php
+++ src/Config.php
@@ -3,6 +3,10 @@ namespace System;
 
 class Config
 {
+	//
+	// MySQL
+	//
+	
 	// SQL server connectiong string
 	// See https://en.wikipedia.org/wiki/Data_source_name
 	public static $Database = 'mysql:host=127.0.0.1;port=3306;dbname=edu_testing;charset=utf8';
@@ -12,4 +16,25 @@ class Config
 	
 	// SQL server password
 	public static $DatabasePassword = '';
+	
+	
+	//
+	// SMTP
+	//
+	
+	// Specify main and backup SMTP servers
+	// Leave empty if you want to use PHP's mail() function
+	public static $MailHost = '';
+	
+	// TCP port to connect to
+	public static $MailPort = 587;
+	
+	// SMTP username
+	public static $MailUsername = '';
+	
+	// SMTP password
+	public static $MailPassword = '';
+	
+	// Enable TLS encryption, `ssl` also accepted
+	public static $MailSecure = 'tls';
 }
