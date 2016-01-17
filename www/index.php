70c14117ef301d001dc803a050cc3900d011bca4
Pavel Djundik <github@xpaw.me>
2016-01-17 14:45:59 +0200

    Include zurb foundation and display a basic login form

--- www/index.php
+++ www/index.php
@@ -3,17 +3,28 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $Klein = new \Klein\Klein();
 
-$Klein->respond( function( $request, $response, $service, $app )
+$Klein->respond( function( $Request, $Response, $Service, $App )
 {
-    $app->register( 'Database', function()
+	$App->register( 'Database', function()
 	{
         return System\Database::Setup();
+	} );
+	
+	$App->register( 'Twig', function()
+	{
+		$Loader = new Twig_Loader_Filesystem( __DIR__ . '/../templates' );
+		$Environment = new Twig_Environment( $Loader,
+		[
+			//'cache' => __DIR__ . '/../templates/cache',
+		] );
+		
+		return $Environment;
     } );
 } );
 
-$Klein->respond( 'GET', '/', function()
+$Klein->respond( 'GET', '/', function( $Request, $Response, $Service, $App )
 {
-	return 'Hello World!';
+	echo $App->Twig->render( 'login.html', [ 'title' => 'Login to ' . \System\Config::$SystemName ] );
 } );
 
 $Klein->respond( 'GET', '/questions', [ 'System\Test', 'DisplayAllQuestions' ] );
