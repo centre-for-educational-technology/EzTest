7d52381778925d962d216a366e6c9aa3d7a8e9be
Pavel Djundik <github@xpaw.me>
2016-03-12 18:19:42 +0200

    Update table schema

--- install/database.sql
+++ install/database.sql
@@ -1,17 +1,10 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.1.9-MariaDB-log - mariadb.org binary distribution
--- Server OS:                    Win32
--- HeidiSQL Version:             9.3.0.4984
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping database structure for edu_testing
-CREATE DATABASE IF NOT EXISTS `edu_testing` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE IF NOT EXISTS `edu_testing` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `edu_testing`;
 
 
@@ -19,8 +12,11 @@ USE `edu_testing`;
 CREATE TABLE IF NOT EXISTS `groups` (
   `GroupID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `GroupName` varchar(64) NOT NULL,
+  `UserID` int(11) unsigned NOT NULL,
   `Date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`GroupID`)
+  PRIMARY KEY (`GroupID`),
+  KEY `FK_groups_users` (`UserID`),
+  CONSTRAINT `FK_groups_users` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
@@ -29,11 +25,11 @@ CREATE TABLE IF NOT EXISTS `groups` (
 -- Dumping structure for table edu_testing.groups_users
 CREATE TABLE IF NOT EXISTS `groups_users` (
   `GroupID` int(11) unsigned NOT NULL,
-  `UserID` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`GroupID`,`UserID`),
-  KEY `FK_groups_users_users` (`UserID`),
+  `StudentID` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`GroupID`,`StudentID`),
+  KEY `FK_groups_users_users` (`StudentID`),
   CONSTRAINT `FK_groups_users_groups` FOREIGN KEY (`GroupID`) REFERENCES `groups` (`GroupID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_groups_users_users` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `FK_groups_users_users` FOREIGN KEY (`StudentID`) REFERENCES `users` (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
@@ -42,15 +38,46 @@ CREATE TABLE IF NOT EXISTS `groups_users` (
 -- Dumping structure for table edu_testing.questions
 CREATE TABLE IF NOT EXISTS `questions` (
   `QuestionID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `Tags` varchar(100) NOT NULL DEFAULT '',
   `Type` varchar(25) NOT NULL,
   `Stimulus` mediumtext NOT NULL,
   `Data` mediumtext NOT NULL,
   `Hash` char(32) NOT NULL,
   `UserID` int(10) unsigned NOT NULL,
+  `Date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`QuestionID`),
   UNIQUE KEY `Hash` (`Hash`),
   KEY `FK_questions_users` (`UserID`),
+  KEY `Date` (`Date`),
   CONSTRAINT `FK_questions_users` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table edu_testing.tests
+CREATE TABLE IF NOT EXISTS `tests` (
+  `TestID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `Tags` varchar(100) CHARACTER SET latin1 DEFAULT '',
+  `Name` varchar(100) CHARACTER SET latin1 DEFAULT '',
+  `UserID` int(11) unsigned DEFAULT NULL,
+  `Date` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`TestID`),
+  KEY `FK_tests_users` (`UserID`),
+  CONSTRAINT `FK_tests_users` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table edu_testing.tests_questions
+CREATE TABLE IF NOT EXISTS `tests_questions` (
+  `TestID` int(11) unsigned DEFAULT NULL,
+  `QuestionID` int(11) unsigned DEFAULT NULL,
+  KEY `FK_tests_questions_questions` (`QuestionID`),
+  KEY `FK_tests_questions_tests` (`TestID`),
+  CONSTRAINT `FK_tests_questions_questions` FOREIGN KEY (`QuestionID`) REFERENCES `questions` (`QuestionID`),
+  CONSTRAINT `FK_tests_questions_tests` FOREIGN KEY (`TestID`) REFERENCES `tests` (`TestID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
@@ -61,8 +88,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `UserID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Email` varchar(64) NOT NULL,
   `Name` varchar(64) DEFAULT NULL,
-  `Password` binary(60) NOT NULL DEFAULT '0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',
-  `IsAdmin` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `Password` varchar(255) NOT NULL,
   PRIMARY KEY (`UserID`),
   UNIQUE KEY `Email` (`Email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
